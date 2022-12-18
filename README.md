012bc8d71407c645a3686eeea2991d98ad96bf31
andrew <andrey.resler@yandex.ru>
2022-12-18 01:51:26 +0100

    init

---
+++ README.md
@@ -0,0 +1,50 @@
+A simple script to download latest master build from TeamCity build server
+
+### Usage
+
+1. Copy `config.example.ini` to `config.ini` and fill the parameters.
+
+Token can be generated in your Profile page >> Access tokens
+
+2. Create virtual environment if necessary and install requirements.
+
+```shell
+> VENV_PATH=~/.virtualenvs
+> python -m venv $VENV_PATH/venv
+> source $VENV_PATH/venv/bin/activate
+> pip install -r requirements.txt
+```
+
+3. Check the script usage: `python main.py -h`
+
+```
+usage: main.py [-h] -b BUILD_TYPE -f FILE_PATTERN [-t TARGET_DIR]
+
+options:
+  -h, --help            show this help message and exit
+  -b BUILD_TYPE, --build-type BUILD_TYPE
+                        Built configuration ID
+  -f FILE_PATTERN, --file-pattern FILE_PATTERN
+                        Artifact file pattern. E.g. pycharmPC-{build}-aarch64.dmg
+  -t TARGET_DIR, --target-dir TARGET_DIR
+                        [OPTIONAL] Download directory. Default is current workdir
+```
+
+`--build-type` -- Can be copied from specific build configuration's settings
+
+![](https://i.imgur.com/Hq9Y8m0.png)
+
+`--file-pattern` -- The name of the artifact to download. Replace the build number with `{build}` placeholder.
+
+E.g. `pycharmPY-{build}-aarch64.dmg`
+
+To download with a single command, add an alias to your shell config.
+
+Example for `.bashrc`/`.zshrc`:
+
+```shell
+alias dlcharm="~/.virtualenvs/venv311/bin/python ~/projects/build-master-downloader/main.py \
+    -b my_awesome_configuration_id \
+    -f pycharmPY-{build}.dmg \
+    -t ~/Downloads"
+```
---
+++ main.py
@@ -0,0 +1,131 @@
+import argparse
+import configparser
+import os
+import requests
+import shutil
+
+
+class InvalidFilePatternError(Exception):
+    def __init__(self, pattern):
+        self.message = f"Invalid file pattern: {pattern}\nPlease make sure it includes {{build}} placeholder"
+        super().__init__(self.message)
+
+
+class TCclient:
+    def __init__(self, config):
+        self.base_url = self.validate_base_url(config['general']['base_url'])
+        self.token = config['general']['token']
+        self.verify = config.getboolean('general', 'ssl_verify')
+
+    @staticmethod
+    def validate_link(link: str) -> str:
+        if not link.startswith('/'):
+            link = '/' + link
+        return link
+
+    @staticmethod
+    def validate_target_dir(target_dir: str) -> str:
+        if not target_dir.endswith('/'):
+            target_dir = target_dir + '/'
+        return target_dir
+
+    @staticmethod
+    def validate_base_url(url: str) -> str:
+        if url.endswith('/'):
+            url = url[:-1]
+        return url
+
+    def get(self, method: str, params: dict = None):
+        base_url = self.base_url
+        token = self.token
+        verify = self.verify
+
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': f'Bearer {token}'
+        }
+
+        method = self.validate_link(method)
+
+        try:
+            r = requests.get(url=base_url + method, headers=headers, params=params, verify=verify)
+        except requests.exceptions.ConnectionError as e:
+            raise e
+
+        if r.status_code == 200:
+            return r.json()
+        else:
+            print(r.text)
+            exit(1)
+
+    def download_file(self, link, file_name, target_dir):
+        base_url = self.base_url
+        token = self.token
+        verify = self.verify
+
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
+
+        link = self.validate_link(link)
+        target_dir = self.validate_target_dir(target_dir)
+
+        print(
+            f'File URL: {base_url + link}\n'
+            f'Downloading file {file_name} to {target_dir}\n\n'
+            f'Download is in progress, please wait until finished...'
+        )
+        with requests.get(base_url + link, stream=True, headers=headers, verify=verify) as r:
+            with open(target_dir + file_name, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+
+        if r.status_code == 200:
+            print('Finished downloading.')
+
+
+def validate_file_pattern(self):
+    pattern = self.file_pattern
+    if '{build}' not in pattern:
+        raise InvalidFilePatternError(pattern)
+
+
+def find_latest_build(builds) -> dict:
+    _list = builds['build']
+
+    latest = max(_list, key=lambda x: x['id'])
+    return latest
+
+
+if __name__ == '__main__':
+    project_dir = os.path.dirname(os.path.abspath(__file__))
+    config = configparser.ConfigParser()
+    config.read(project_dir + '/config.ini')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--build-type", type=str, required=True,
+                        help="Built configuration ID")
+    parser.add_argument("-f", "--file-pattern", type=str, required=True,
+                        help="Artifact file pattern. E.g. pycharmPC-{build}-aarch64.dmg")
+    parser.add_argument("-t", "--target-dir", default=os.getcwd(), type=str, required=False,
+                        help="[OPTIONAL] Download directory. Default is current workdir")
+    args = parser.parse_args()
+
+    clnt = TCclient(config)
+
+    build_type: str = args.build_type
+    file_pattern: str = args.file_pattern
+    target_dir: str = args.target_dir
+
+    builds = clnt.get(
+        method=f'/app/rest/buildTypes/id:{build_type}/builds/',
+        params={'locator': 'branch:master,status:success,state:finished'}
+    )
+
+    latest_build = find_latest_build(builds)
+
+    build_id: int = latest_build['id']
+    build_number: str = latest_build['number']
+
+    file_name = file_pattern.replace('{build}', build_number)
+    file_link = f'/app/rest/builds/id:{build_id}/artifacts/content/{file_name}'
+    clnt.download_file(file_link, file_name, target_dir)
